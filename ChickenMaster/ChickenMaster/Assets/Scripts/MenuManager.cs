86d53db65a19ff296867118921c24c962d3fbb70
Maria Gembarzewska <idziemy_spac_billy@hotmail.com>
2019-04-28 01:22:21 +0200

    zmiany maksia 2

---
+++ ChickenMaster/ChickenMaster/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip hover;
+    public AudioClip click;
+
+    public void loadNextScene()
+    {
+        Application.LoadLevel("OtherScene");
+    }
+     
+    public void OnHover()
+    {
+        source.PlayOneShot(hover);
+    }
+    
+    public void OneClick()
+    {
+        source.PlayOneShot(click);
+
+    }
+
+   
+}
---
+++ ChickenMaster/ChickenMaster/Assets/Scripts/MoveingPath.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveingPath : MonoBehaviour
+{
+    public enum PathTypes //Types of movement paths
+    {
+        linear,
+        loop
+    }
+
+    public PathTypes PathType; //Idicates type of path (Linear or Looping)
+    public int movementDirection = 1; //1 clockwise/forward ||-1counter clockwise/backwards
+    public int movingTo = 0; //used to identyfy point in PathSequence we are moving to 
+    public Transform[] PathSequence; //Arry of all point in the path
+
+
+    private void Update()
+    {
+
+    }
+    public void OnDrawGizmos()
+    {
+        if (PathSequence == null || PathSequence.Length < 2)
+        {
+            return;
+        }
+
+
+
+        for (var i = 1; i < PathSequence.Length; i++)
+
+        {
+            Gizmos.DrawLine(PathSequence[i - 1].position, PathSequence[i].position);
+
+        }
+
+        {
+            if (PathType == PathTypes.loop) ;
+
+        }
+        Gizmos.DrawLine(PathSequence[0].position, PathSequence[PathSequence.Length - 1].position);
+    }
+    public IEnumerator<Transform> GetNextPathPoint()
+    {
+        if (PathSequence == null || PathSequence.Length < 1)
+        {
+            yield break;
+        }
+        while (true)
+        {
+        }
+    }
+}
+
